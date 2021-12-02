e30ec6e8c820ea8b1ffda751577352acb9e099de
Sergey <sergeychits20@gmail.com>
2021-12-01 19:53:20 -0500

    Added Huffman tree file

---
+++ HuffmanTree.py
@@ -0,0 +1,46 @@
+class HuffmanTree:
+    """
+    A class to represent an Huffman Tree
+    Attributes
+    ----------
+    char : str
+        The characters represented by this tree
+    count : int
+        The count of how many times char occurred in the text
+    left : HuffmanTree/None
+        The left HuffmanTree below this one
+    right : HuffmanTree/None
+        The right HuffmanTree below this one
+    bit : bool
+        The bit symbol used to reach this HuffmanTree (either True/False for 1/0)
+
+
+    General Structure
+                         HuffmanTree (char,count,bit)
+                          /    \
+                      left    right
+                        /        \
+                HuffmanTree      HuffmanTree
+
+    Default Structure
+                         HuffmanTree (char,count,None)
+                          /    \
+                      left    right
+                        /        \
+                     None         None
+    """
+
+    # PART1 (constructor)
+
+    # PART2 (order)
+
+    # PART3 (string)
+
+    # PART3 (representation)
+
+    # PART5 (equality)
+
+# PART1 (make_trees)
+
+
+# PART4 (merge)
